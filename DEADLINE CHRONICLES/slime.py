543741ae5cebda7ffab244c2db49792ceef575dd
leonardocatli <leonardocatli459@gmail.com>
2025-06-01 18:18:09 +0800

    updated

--- DEADLINE CHRONICLES/slime.py
+++ DEADLINE CHRONICLES/slime.py
@@ -28,6 +28,8 @@ BIRD = [pygame.image.load(os.path.join("Assets/Bird", "Bird1.png")),
 CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
 BG = pygame.image.load(os.path.join("Assets/Other", "grass.png"))
 
+MENU_BG = pygame.image.load(os.path.join("Assets/BG", "Wild_West.JPG"))
+
 JUMP_SFX = pygame.mixer.Sound(os.path.join("Assets/sounds", "jump.wav"))
 JUMP_SFX.set_volume(1)
 
@@ -38,7 +40,7 @@ COIN_SFX = pygame.mixer.Sound(os.path.join("Assets/sounds", "point.wav"))
 COIN_SFX.set_volume(1)
 
 BG_MUSIC_PATH = os.path.join("Assets/sounds", "western.mp3")
-DEATH_MUSIC_PATH = os.path.join("Assets/sounds", "death.mp3")
+DEATH_MUSIC_PATH = os.path.join("Assets/sounds", "game_over.mp3")
 MENU_MUSIC_PATH = os.path.join("Assets/sounds", "desert.mp3")
 VICTORY_MUSIC_PATH = os.path.join("Assets/sounds", "death.mp3")
 
@@ -135,7 +137,7 @@ class Dinosaur:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-        pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
+        # pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
 
 class Cloud:
     def __init__(self):
@@ -151,6 +153,7 @@ class Cloud:
             self.y = random.randint(50, 100)
 
     def draw(self, SCREEN):
+
         SCREEN.blit(self.image, (self.x, self.y))
 
 
@@ -178,6 +181,21 @@ class Background:
         SCREEN.blit(self.image, (self.x1, self.y))
         SCREEN.blit(self.image, (self.x2, self.y))
 
+class Menu_Background:
+    def __init__(self):
+
+        self.bgmenu = MENU_BG
+        self.image_width = self.bgmenu.get_width()
+        self.x1 = 0
+        self.x2 = self.image_width
+        self.y = 5
+
+    def update(self):
+        pass
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.bgmenu, (self.x1, self.y))
+
 
 class Obstacle:
 
@@ -197,7 +215,7 @@ class Obstacle:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image[self.type], self.rect)
-        pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
+     #   pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
 
 
 class SmallCactus(Obstacle):
@@ -235,7 +253,7 @@ class Bird(Obstacle):
         if self.index >= 9:
             self.index = 0
         SCREEN.blit(self.image[self.index // 5], self.rect)
-        pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
+        #pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
         self.index += 1
 
 
@@ -257,7 +275,7 @@ class Coin:
     def draw(self, screen):
         if not self.collected:
             screen.blit(self.image, self.rect)
-            pygame.draw.rect(screen, (255, 215, 0), self.rect, 1)
+          #  pygame.draw.rect(screen, (255, 215, 0), self.rect, 1)
 
 
 def load_highscore():
@@ -283,6 +301,8 @@ def main():
     player = Dinosaur()
     cloud = Cloud()
     background_img = Background()
+
+
     points = 0
     game_speed = 20
     x_pos_bg = 0
@@ -361,7 +381,7 @@ def main():
                 SCREEN.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2,
                                              SCREEN_HEIGHT // 3 - game_over_text.get_height() // 3))
                 pygame.display.update()
-                pygame.time.delay(4000)
+                pygame.time.delay(5500)
 
                 death_count += 1
                 if points > load_highscore():
@@ -387,7 +407,7 @@ def main():
             if not coin.collected and coin.rect.right > 0:
                 active_coins += 1
 
-        if collected_coins == 5:
+        if collected_coins == 1:
             mixer.music.stop()
             mixer.music.load(VICTORY_MUSIC_PATH)
             mixer.music.set_volume(0.5)
@@ -396,35 +416,39 @@ def main():
             victory_text = font.render("VICTORY!", True, (0, 200, 0))
             SCREEN.blit(victory_text, (SCREEN_WIDTH // 2 - victory_text.get_width() // 2,
                                        SCREEN_HEIGHT // 3 - victory_text.get_height() // 2))
-
-            option_font = pygame.font.Font(FONT_PATH, 20)
-            continue_text = option_font.render("Press C to Continue", True, (0, 0, 0))
-            menu_text = option_font.render("Press M for Menu", True, (0, 0, 0))
-            SCREEN.blit(continue_text, (SCREEN_WIDTH // 2 - continue_text.get_width() // 2, SCREEN_HEIGHT // 2 + 30))
-            SCREEN.blit(menu_text, (SCREEN_WIDTH // 2 - menu_text.get_width() // 2, SCREEN_HEIGHT // 2 + 70))
-
             pygame.display.update()
+            pygame.time.delay(7000)
+            pygame.quit()
 
-            waiting = True
-            while waiting:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        waiting = False
-                        run = False
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_c:
-                            collected_coins = 0
-                            coins.clear()
-                            mixer.music.load(BG_MUSIC_PATH)
-                            mixer.music.set_volume(0.5)
-                            mixer.music.play(-1)
-                            waiting = False
-
-                        elif event.key == pygame.K_m:
-                            menu(death_count)
-                            waiting = False
-                            return
+            #
+            # option_font = pygame.font.Font(FONT_PATH, 20)
+            # continue_text = option_font.render("Press C to Continue", True, (0, 0, 0))
+            # menu_text = option_font.render("Press M for Menu", True, (0, 0, 0))
+            # SCREEN.blit(continue_text, (SCREEN_WIDTH // 2 - continue_text.get_width() // 2, SCREEN_HEIGHT // 2 + 30))
+            # SCREEN.blit(menu_text, (SCREEN_WIDTH // 2 - menu_text.get_width() // 2, SCREEN_HEIGHT // 2 + 70))
+            #
+            # pygame.display.update()
+            #
+            # waiting = True
+            # while waiting:
+            #     for event in pygame.event.get():
+            #         if event.type == pygame.QUIT:
+            #             pygame.quit()
+            #             waiting = False
+            #             run = False
+            #         if event.type == pygame.KEYDOWN:
+            #             if event.key == pygame.K_c:
+            #                 collected_coins = 0
+            #                 coins.clear()
+            #                 mixer.music.load(BG_MUSIC_PATH)
+            #                 mixer.music.set_volume(0.5)
+            #                 mixer.music.play(-1)
+            #                 waiting = False
+            #
+            #             elif event.key == pygame.K_m:
+            #                 menu(death_count)
+            #                 waiting = False
+            #                 return
 
         if coins_spawned_for_score and active_coins == 0:
             next_coin_spawn_score += 300
@@ -453,11 +477,12 @@ def menu(death_count):
     clock = pygame.time.Clock()
     step_index = 0
 
+    menu_bg = Menu_Background()
     font = pygame.font.Font(FONT_PATH, 20)
 
     while run:
         SCREEN.fill((255, 255, 255))
-
+        menu_bg.draw(SCREEN)
 
         title = font.render("THE BRAINSTORMING DUNGEON!", True, (0, 0, 0))
         SCREEN.blit(title, (SCREEN_WIDTH // 2 - title.get_width() // 2, SCREEN_HEIGHT // 2 - 190))
